c9e139308d2d1f0b387c60617275eefb0950bc8e
AYehia0 <ahmedyehia611@gmail.com>
2021-04-06 23:50:50 +0200

    make shitty help menu

--- doit.c
+++ doit.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(){
 
-    printf("InitVersion");
+void helpMenu(){
+    /*Main Menu, which contains help arguments*/        
+    printf("ToDo app to orginize you time\n");
+    printf("Usage: doit [OPTION] \n");
+    printf("Options:\n");
+    printf("\t-a, --action\t\tAdd Event in the list\n");
+    printf("\t-d, --delete-action\tDeletes an event from the list\n");
+}
+
+int main(int argc, char *argv[]){
+    if(argv[1]!=NULL){
+        printf("not null");
+
+    }else{
+        helpMenu();
+    }
+
+    return 0;
 }

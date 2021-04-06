3537f41b4b866be73e262bbb67697da667ca5868
AYehia0 <ahmedyehia611@gmail.com>
2021-04-07 00:42:46 +0200

    getting today's date

--- doit.c
+++ doit.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
+#define PATH "~/"
 
 void helpMenu(){
     /*Main Menu, which contains help arguments*/        
@@ -12,12 +14,36 @@ void helpMenu(){
     printf("\t-d, --delete-action\tDeletes an event from the list\n");
 }
 
+char *getTodayDate(){
+    /*Get Today's Date*/
+
+    char * date;
+    date = malloc(sizeof(char)*50);
+    time_t rawTime;
+
+    struct tm *timeInfo;
+
+    time(&rawTime);
+    timeInfo = localtime(&rawTime);
+    strftime(date, strlen("DD-MMM-YYYY")+1, "%d-%b-%Y", timeInfo);
+
+    return date;
+
+}
+
+void saveToFile(){
+
+}
+
 int main(int argc, char *argv[]){
     if(argv[1]!=NULL){
         printf("not null");
 
     }else{
         helpMenu();
+        char * d; 
+        d = getTodayDate();
+       printf("%s", d);
     }
 
     return 0;

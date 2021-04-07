c40afb1f0d12e23cce21f675eee5310240a1a442
AYehia0 <ahmedyehia611@gmail.com>
2021-04-07 23:39:42 +0200

    adding Date at the beginning of the file

--- doit.c
+++ doit.c
@@ -14,9 +14,10 @@ void helpMenu(){
     printf("\t-d, --delete-action\tDeletes an event from the list\n");
 }
 
-char *getTodayDate(){
-    /*Get Today's Date*/
+char *getTodayDate(char whichTime[1]){
 
+    /*Get Today's Date*/
+ 
     char * date;
     date = malloc(sizeof(char)*50);
     time_t rawTime;
@@ -25,20 +26,50 @@ char *getTodayDate(){
 
     time(&rawTime);
     timeInfo = localtime(&rawTime);
-    strftime(date, strlen("DD-MMM-YYYY")+1, "%d-%b-%Y", timeInfo);
+
+   
+    // Day Calender
+    if(strcmp("d", whichTime) == 0){
+        
+        strftime(date, strlen("DD-MMM-YYYY")+1, "%d-%b-%Y", timeInfo);
+        
+    }else{
+        //must be Time in hours
+
+        strftime(date, strlen("HH:MM")+1, "%H:%M", timeInfo);
+    }
 
     return date;
 
 }
 
 void saveToFile(char *event ){
+    char * date;
+    
     FILE *pFile;
-    pFile = fopen("Test", "w");
+    pFile = fopen("Test", "a");
+
+    //Getting the date
+    date = getTodayDate("d");
+    
 
     //checking for file
     if(pFile == NULL){
         printf("Error\n");
     }else{
+
+        //checking if File is empty 
+        fseek(pFile, 0, SEEK_END);
+
+        if(ftell(pFile) == 0){
+
+            //Adding toDay's date
+            fputs("########## ", pFile);
+            fputs(date, pFile);
+            fputs(" ##########\n", pFile);  
+
+
+        }
 
         //Writing to the file
         if(strlen(event) > 0){
@@ -48,7 +79,7 @@ void saveToFile(char *event ){
             fputs(",", pFile);
 
             //Adding a newline
-            fputs("\n", pFile);
+            fputs(" ", pFile);
         }
         //Closing the file
         fclose(pFile);
@@ -65,16 +96,19 @@ int main(int argc, char *argv[]){
         if(strcmp("-a", argv[1])==0 || strcmp("--action", argv[1])==0){
 
             //Checking for the second arg
-            if(strlen(argv[2]) > 0){
+            if(argv[2] != NULL){
                 //Writing to the file
+
+                //Writing the date
                 saveToFile(argv[2]);
+
+            }else{
+                printf("Invalid Input, What do you want to do?\n");
+                helpMenu();
             }
         }
     }else{
         helpMenu();
-        char * d; 
-        d = getTodayDate();
-       printf("%s", d);
     }
 
     return 0;

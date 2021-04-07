bbd64663a5319b94bc3a2537426e8a9b4a1a9a69
AYehia0 <ahmedyehia611@gmail.com>
2021-04-07 22:52:55 +0200

    writing to a file [TESTING]

--- doit.c
+++ doit.c
@@ -31,14 +31,45 @@ char *getTodayDate(){
 
 }
 
-void saveToFile(){
+void saveToFile(char *event ){
+    FILE *pFile;
+    pFile = fopen("Test", "w");
 
+    //checking for file
+    if(pFile == NULL){
+        printf("Error\n");
+    }else{
+
+        //Writing to the file
+        if(strlen(event) > 0){
+            fputs(event, pFile);
+
+            //adding , to seprate 
+            fputs(",", pFile);
+
+            //Adding a newline
+            fputs("\n", pFile);
+        }
+        //Closing the file
+        fclose(pFile);
+    }
+    
 }
 
 int main(int argc, char *argv[]){
     if(argv[1]!=NULL){
-        printf("not null");
 
+        //Checking for args
+
+        //checking for appending
+        if(strcmp("-a", argv[1])==0 || strcmp("--action", argv[1])==0){
+
+            //Checking for the second arg
+            if(strlen(argv[2]) > 0){
+                //Writing to the file
+                saveToFile(argv[2]);
+            }
+        }
     }else{
         helpMenu();
         char * d; 

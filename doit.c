0d44247db7883f08a130727fed015e4656feb510
AYehia0 <ahmedyehia611@gmail.com>
2021-04-09 18:40:53 +0200

    getting time input from user and check for validation

--- doit.c
+++ doit.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define PATH "~/"
 
@@ -14,6 +15,46 @@ void helpMenu(){
     printf("\t-d, --delete-action\tDeletes an event from the list\n");
 }
 
+bool checkValidTime(int *timeArr){
+    /*Check if the time is in a valid form*/
+ 
+    int hours = timeArr[0];
+    int mins = timeArr[1];
+    
+    if(hours <= 12 && hours >= 1 && mins > -1 && mins <= 59){
+        return true;    
+    }
+
+    return false;
+
+}
+
+int *getTimeInput(char *timeStr){
+    /*Process the time given from the user*/
+    
+    //Array to save the given Time
+    int *timeContainer = malloc(sizeof(int)*4); 
+    
+    char temp[20];
+    //int timeContainer[2];
+
+    //Spliting on :
+    char *token = strtok(timeStr, ":");
+   
+    //Counter just in case
+    int coStrLen = 0; 
+    
+    while(token != NULL){
+        strcpy(temp, token);
+        timeContainer[coStrLen++] = atoi(temp);
+    
+        //pointer to the next index for the token
+        token = strtok(NULL, ":");
+
+    }
+    return timeContainer;
+
+}
 char *getTodayDate(char whichTime[1]){
 
     /*Get Today's Date*/
@@ -95,8 +136,21 @@ void saveToFile(char *event ){
 
 int main(int argc, char *argv[]){
     if(argv[1]!=NULL){
+        
+        //temp var from args
 
+        char *tempArg = argv[1];
         //Checking for args
+        
+        printf("You give : %s\n", argv[1]);
+        if (checkValidTime(getTimeInput(tempArg))){
+            printf("Valid Time: %s\n", argv[1]);
+        }else{
+            printf("Invalid Time: %s\n", argv[1]);
+        }
+
+        printf("You give : %s\n", tempArg);
+
 
         //checking for appending
         if(!strcmp("-a", argv[1])==0 || !strcmp("--action", argv[1])==0){
